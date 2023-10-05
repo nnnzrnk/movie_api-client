a3d1164ebce1b4af5039ad268f07e53f231e9df3
nnnzrnk <127133520+nnnzrnk@users.noreply.github.com>
2023-10-05 11:16:48 +0200

    completed 3.5

--- src/components/main-view/main-view.jsx
+++ src/components/main-view/main-view.jsx
@@ -1,13 +1,21 @@
 import { useEffect, useState } from "react";
 import { MovieCard } from "../movie-card/movie-card";
 import { MovieView } from "../movie-view/movie-view";
+import { LoginView } from "../login-view/login-view";
+import { SignupView } from "../signup-view/signup-view";
 
 export const MainView = () => {
+  const storedUser = JSON.parse(localStorage.getItem('user'))
+  const storedToken = localStorage.getItem('token')
   const [movies, setMovies] = useState([]);
-
   const [selectedMovie, setSelectedMovie] = useState(null);
+  const [user, setUser] = useState(storedUser ? storedUser : null);
+  const [token, setToken] =  useState(storedToken ? storedToken : null)
 
   useEffect(() => {
+    if(!token) {
+      return
+    }
     fetch('https://movie-api-da5i.onrender.com/movies')
     .then((response) => response.json())
     .then((data) => {
@@ -24,8 +32,23 @@ export const MainView = () => {
       })
       setMovies(movieFromApi)
       })
-    }, [])
+    }, [token])
   
+
+    if (!user) {
+      return (
+        <>
+          <LoginView
+            onLoggedIn={(user, token) => {
+              setUser(user);
+              setToken(token);
+            }}
+          />
+          or
+          <SignupView />
+        </>
+      );
+    }
 
   if (selectedMovie) {
     return (
@@ -35,7 +58,6 @@ export const MainView = () => {
       />
     );
   }
-
   if (movies.length === 0) {
     return <div>The list is empty!</div>;
   }
@@ -53,6 +75,17 @@ export const MainView = () => {
           />
         );
       })}
+
+      <button
+        onClick={() => {
+          setUser(null);
+          setToken(null);
+          localStorage.clear()
+        }}
+      >
+        Logout
+      </button>
+
     </div>
   );
 };
